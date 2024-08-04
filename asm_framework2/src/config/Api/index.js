9b44058f87746bc9e8d2157adb2f3969f2139b94
phattnt <thanhphat200415@gmail.com>
2024-08-04 21:21:31 +0700

    src

---
+++ asm_framework2/src/config/Api/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { Cookies } from "react-cookie";
+
+// Đặt URL cơ bản cho API của bạn
+const BASE_URL = "http://localhost:5000/api/user/list";
+
+const request = async ({
+  method = "GET",
+  path = "",
+  data = {},
+  headers = {},
+}) => {
+  try {
+    const cookie = new Cookies();
+    const token = cookie.get("token");
+
+    const res = await axios({
+      method: method,
+      baseURL: BASE_URL, // Sử dụng URL cơ bản
+      url: path, // Thêm phần path vào URL cơ bản
+      data: data,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        ...headers,
+      },
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    alert(err?.response?.data?.message);
+    return null;
+  }
+};
+
+export default request;
